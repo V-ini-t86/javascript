5158f9ce36834350cb9c686a8b8c25fcedeedbb7
V-ini-t86 <vinitjoshi.2345@gmail.com>
2022-01-11 22:30:31 +0530

    promise.all

--- promise.js
+++ promise.js
@@ -1,47 +1,49 @@
-function getSongs() {
-  return new Promise((res, rej) => {
-    let singer = getFamousSinger();
-    setTimeout(() => {
-      res(singer);
-    }, 2000);
-  });
-}
+// Promise.all method;
 
-function getFamousSinger() {
-  return 'Eminem';
-}
+// function getSongs() {
+//   return new Promise((res, rej) => {
+//     let singer = getFamousSinger();
+//     setTimeout(() => {
+//       res(singer);
+//     }, 2000);
+//   });
+// }
 
-function getSongsList() {
-  return new Promise((res, rej) => {
-    let list = ['Song A', 'Song B', 'Song C'];
-    setTimeout(() => {
-      res(list);
-    }, 2000);
-  });
-}
+// function getFamousSinger() {
+//   return 'Eminem';
+// }
 
-function getFamousSong() {
-  return new Promise((res, rej) => {
-    let famousSong = 'Song C';
-    setTimeout(() => {
-      res(famousSong);
-    }, 2000);
-  });
-}
+// function getSongsList() {
+//   return new Promise((res, rej) => {
+//     let list = ['Song A', 'Song B', 'Song C'];
+//     setTimeout(() => {
+//       res(list);
+//     }, 2000);
+//   });
+// }
 
-getSongs()
-  .then((singer) => {
-    console.log(singer);
-    return getSongsList();
-  })
-  .then((list) => {
-    console.log(list);
-    return getFamousSong();
-  })
-  .then((song) => {
-    console.log(song);
-  })
-  .catch((err) => console.log(err));
+// function getFamousSong() {
+//   return new Promise((res, rej) => {
+//     let famousSong = 'Song C';
+//     setTimeout(() => {
+//       res(famousSong);
+//     }, 2000);
+//   });
+// }
+
+// getSongs()
+//   .then((singer) => {
+//     console.log(singer);
+//     return getSongsList();
+//   })
+//   .then((list) => {
+//     console.log(list);
+//     return getFamousSong();
+//   })
+//   .then((song) => {
+//     console.log(song);
+//   })
+//   .catch((err) => console.log(err));
 
 // function createPromise() {
 //   return new Promise((resolve, reject) => {
